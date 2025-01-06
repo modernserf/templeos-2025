960138ee19dd93fb7db5c42b74660ed4f6954d21
Justin Falcone <justin@justinfalcone.com>
2025-01-05 21:04:21 -0500

    redesign database, add tests

---
+++ src/db.test.ts
@@ -0,0 +1,73 @@
+import { expect, test } from "vitest";
+import { DB, q } from "./db";
+
+test("basic queries", () => {
+  const db = new DB();
+  db.bulkInsert({
+    foo: { value: 123 },
+    bar: { baz: 456, quux: 789 },
+  });
+  const query = q("foo", "bar")
+    .get("foo", "value", "value")
+    .get("bar", "baz", "baz")
+    .get("bar", "quux", "quux");
+  expect(db.query1(query, { foo: "foo", bar: "bar" })).toEqual({
+    foo: "foo",
+    bar: "bar",
+    value: 123,
+    baz: 456,
+    quux: 789,
+  });
+});
+
+test("indexes", () => {
+  const db = new DB();
+  db.bulkInsert({
+    foo: { value: 123, idx: "hello" },
+    bar: { value: 456, idx: "hello" },
+    baz: { value: 789, idx: "goodbye" },
+  });
+  db.createIndex("idx");
+  const query = q("key") //
+    .index("id", "idx", "key")
+    .get("id", "value", "value");
+  expect([...db.queryAll(query, { key: "hello" })]).toEqual([
+    { id: "foo", key: "hello", value: 123 },
+    { id: "bar", key: "hello", value: 456 },
+  ]);
+  expect([...db.queryAll(query, { key: "goodbye" })]).toEqual([
+    { id: "baz", key: "goodbye", value: 789 },
+  ]);
+});
+
+test("update", () => {
+  const db = new DB();
+  db.bulkInsert({
+    foo: { value: 123, idx: "hello" },
+    bar: { value: 456, idx: "hello" },
+    baz: { value: 789, idx: "goodbye" },
+  });
+  db.createIndex("idx");
+  let didChange = false;
+  db.addEventListener(() => {
+    didChange = true;
+  });
+
+  expect(didChange).toBe(false);
+
+  const update = q("id", "key") //
+    .update("id", "idx", "key");
+  db.update(update, { id: "foo", key: "goodbye" });
+
+  expect(didChange).toBe(true);
+
+  const query = q("key") //
+    .index("id", "idx", "key")
+    .get("id", "value", "value");
+  expect(new Set(db.queryAll(query, { key: "goodbye" }))).toEqual(
+    new Set([
+      { id: "foo", key: "goodbye", value: 123 },
+      { id: "baz", key: "goodbye", value: 789 },
+    ])
+  );
+});
---
+++ src/db.ts
@@ -0,0 +1,176 @@
+type Id = string;
+type Field = string;
+
+type Rec = Record<Field, unknown>;
+type Idx = Record<Field, Set<Id>>;
+
+type Ident = string;
+export type Query = {
+  params: Ident[];
+  items: QueryItem[];
+};
+
+type QueryItem =
+  | { tag: "get"; id: Ident; field: Field; value: Ident }
+  | { tag: "index"; id: Ident; field: Field; value: Ident }
+  | { tag: "update"; id: Ident; field: Field; value: Ident }
+  | { tag: "deleteField"; id: Ident; field: Field }
+  | { tag: "deleteRecord"; id: Ident };
+
+export type QueryArgs = Record<Ident, unknown>;
+
+class QueryBuilder implements Query {
+  items: QueryItem[] = [];
+  constructor(public params: Ident[]) {}
+  get(id: Ident, field: Field, value: Ident) {
+    this.items.push({ tag: "get", id, field, value });
+    return this;
+  }
+  index(id: Ident, field: Field, value: Ident) {
+    this.items.push({ tag: "index", id, field, value });
+    return this;
+  }
+  update(id: Ident, field: Field, value: Ident) {
+    this.items.push({ tag: "update", id, field, value });
+    return this;
+  }
+}
+
+export const q = (...params: Ident[]) => new QueryBuilder(params);
+
+function expectVar<T>(args: QueryArgs, ident: Ident): T {
+  const value = args[ident];
+  if (!value) throw new Error();
+  return value as T;
+}
+
+function checkVar<T>(args: QueryArgs, ident: Ident, value: T) {
+  if (!value) throw new Error();
+  if (ident in args) {
+    if (args[ident] !== value) throw new Error();
+  } else {
+    args[ident] = value;
+  }
+  return value;
+}
+
+export class DB {
+  private data = new Map<Id, Rec>();
+  private index = new Map<Id, Idx>();
+  private indexedFields = new Set<Field>();
+  private eventListeners: Array<() => void> = [];
+  bulkInsert(items: Record<Id, Rec>) {
+    for (const [id, rec] of Object.entries(items)) {
+      this.insertRec(id, rec);
+    }
+    this.notifyEventListeners();
+  }
+  query1(query: Query, args: QueryArgs) {
+    const iter = this.runQuery(query, { ...args });
+    return iter.next().value;
+  }
+  queryAll(query: Query, args: QueryArgs) {
+    return this.runQuery(query, { ...args });
+  }
+  update(query: Query, args: QueryArgs) {
+    for (const _ of this.runQuery(query, { ...args })) {
+      // empty
+    }
+    this.notifyEventListeners();
+  }
+  createIndex(field: Field) {
+    this.indexedFields.add(field);
+    for (const [id, rec] of this.data) {
+      if (field in rec) {
+        this.addToIndex(id, field, rec[field] as Id);
+      }
+    }
+  }
+  addEventListener(fn: () => void) {
+    this.eventListeners.push(fn);
+    return () => {
+      this.eventListeners = this.eventListeners.filter((f) => f !== fn);
+    };
+  }
+  private notifyEventListeners() {
+    for (const l of this.eventListeners) {
+      l();
+    }
+  }
+  private insertRec(id: Id, rec: Rec) {
+    this.data.set(id, rec);
+    for (const [field, value] of Object.entries(rec)) {
+      if (this.indexedFields.has(field)) {
+        this.addToIndex(id, field, value as Id);
+      }
+    }
+    if (rec.db__schema === "schema__index" && rec.index__field) {
+      this.createIndex(rec.index__field as Field);
+    }
+  }
+  private *runQuery(
+    query: Query,
+    args: QueryArgs,
+    index = 0
+  ): Generator<QueryArgs, undefined, undefined> {
+    if (index >= query.items.length) {
+      yield args;
+      return;
+    }
+    const q = query.items[index];
+    switch (q.tag) {
+      case "get": {
+        const id = expectVar<Id>(args, q.id);
+        const record = this.data.get(id);
+        if (!record) throw new Error();
+        checkVar(args, q.value, record[q.field]);
+        yield* this.runQuery(query, args, index + 1);
+        return;
+      }
+      case "index": {
+        const value = expectVar<Id>(args, q.value);
+        const idx = this.index.get(value);
+        if (!idx) throw new Error();
+        const ids = idx[q.field];
+        if (!ids) throw new Error();
+        for (const id of ids) {
+          const nextArgs = { ...args };
+          checkVar(nextArgs, q.id, id);
+          yield* this.runQuery(query, nextArgs, index + 1);
+        }
+        return;
+      }
+      case "update": {
+        const id = expectVar<Id>(args, q.id);
+        const value = expectVar(args, q.value);
+        let record = this.data.get(id);
+        if (!record) {
+          record = {};
+          this.data.set(id, record);
+        }
+        const prevValue = record[q.field];
+        record[q.field] = value;
+        if (this.indexedFields.has(q.field)) {
+          if (prevValue) this.removeFromIndex(id, q.field, prevValue as Id);
+          this.addToIndex(id, q.field, value as Id);
+        }
+
+        yield* this.runQuery(query, args, index + 1);
+      }
+    }
+  }
+  private addToIndex(id: Id, field: Field, value: Id) {
+    let idx = this.index.get(value);
+    if (!idx) {
+      idx = {};
+      this.index.set(value, idx);
+    }
+    idx[field] ??= new Set();
+    idx[field].add(id);
+  }
+  private removeFromIndex(id: Id, field: Field, value: Id) {
+    const idx = this.index.get(value);
+    if (!idx) return;
+    idx[field]?.delete(id);
+  }
+}
