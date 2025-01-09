f301fdac789111c522b33d4a7dd995bcfdd1fa81
Justin Falcone <justin@justinfalcone.com>
2025-01-09 08:03:42 -0500

    more real BTree

--- src/btree.test.ts
+++ src/btree.test.ts
@@ -1,69 +1,48 @@
 import { expect, test } from "vitest";
+import { asc, desc, Tree } from "./btree";
 
-type K = number;
+test("next tree", () => {
+  const tree = new Tree<number, number>(asc);
+  const COUNT = 100;
 
-type BTreeNode<V> =
-  | { tag: "leaf"; entries: [K, V][] }
-  | { tag: "subtree"; left: BTree<V>; split: K; right: BTree<V> };
-
-class BTree<V> {
-  constructor(private node: BTreeNode<V>) {}
-  static empty() {
-    return new BTree({ tag: "leaf", entries: [] });
-  }
-  *entries(): Generator<[K, V]> {
-    switch (this.node.tag) {
-      case "leaf":
-        yield* this.node.entries;
-        return;
-      case "subtree":
-        yield* this.node.left.entries();
-        yield* this.node.right.entries();
-    }
-  }
-  insert(key: K, value: V) {
-    switch (this.node.tag) {
-      case "leaf": {
-        for (const [i, [k]] of this.node.entries.entries()) {
-          if (key < k) {
-            const left = this.node.entries.slice(0, i);
-            left.push([key, value]);
-            const right = this.node.entries.slice(i);
-
-            this.node = {
-              tag: "subtree",
-              left: new BTree({ tag: "leaf", entries: left }),
-              split: k,
-              right: new BTree({ tag: "leaf", entries: right }),
-            };
-
-            return;
-          }
-        }
-        this.node.entries.push([key, value]);
-        return;
-      }
-      case "subtree": {
-        if (key < this.node.split) {
-          this.node.left.insert(key, value);
-        } else {
-          this.node.right.insert(key, value);
-        }
-      }
-    }
-  }
-}
-
-test("simple btree", () => {
-  const tree = BTree.empty();
-
-  for (let i = 0; i < 100; i++) {
-    tree.insert(Math.random(), i);
+  for (let i = 0; i < COUNT; i++) {
+    tree.insert(Math.floor(Math.random() * 1000), i);
   }
 
   const items = Array.from(tree.entries());
   const sorted = items.slice().sort(([k0], [k1]) => k0 - k1);
 
-  expect(items.length).toBe(100);
+  expect(items.length).toBe(COUNT);
   expect(items).toEqual(sorted);
+});
+
+test("desc tree", () => {
+  const tree = new Tree<number, number>(desc);
+  const COUNT = 100;
+
+  for (let i = 0; i < COUNT; i++) {
+    tree.insert(Math.floor(Math.random() * 1000), i);
+  }
+
+  const items = Array.from(tree.entries());
+  const sorted = items.slice().sort(([k0], [k1]) => k1 - k0);
+
+  expect(items.length).toBe(COUNT);
+  expect(items).toEqual(sorted);
+});
+
+test("range", () => {
+  const tree = new Tree<number, number>(asc);
+  const COUNT = 1000;
+
+  for (let i = 0; i < COUNT; i++) {
+    tree.insert(Math.floor(Math.random() * 1000), i);
+  }
+
+  const items = Array.from(tree.range(250, 750));
+  const filtered = Array.from(tree.entries())
+    .filter(([k]) => 250 <= k && k <= 750)
+    .sort(([k0], [k1]) => k0 - k1);
+
+  expect(items.map(([k]) => k)).toEqual(filtered.map(([k]) => k));
 });
